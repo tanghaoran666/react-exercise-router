f238588cd4b5776939cd687b44b2f10ffb086790
tanghaoran <407458281@qq.com>
2020-09-24 08:47:57 +0800

    [thr] implement exercise3

--- src/exercise-1/components/App.js
+++ src/exercise-1/components/App.js
@@ -1,6 +1,7 @@
 import React, {Component} from 'react';
 import '../styles/App.css';
-import {BrowserRouter as Router,Route,Switch} from 'react-router-dom';
+import {BrowserRouter as Router,Redirect,Route,Switch} from 'react-router-dom';
+import NoMatch from 'react-router-nomatch'
 import Home from './Home';
 import MyProfile from './MyProfile';
 import AboutUs from './AboutUs';
@@ -9,6 +10,8 @@ import Products from './Products';
 import Product from './Product';
 
 class App extends Component {
+  
+  
   render() {
     const products = [{id:1,
       name:'Bicycle',
@@ -18,16 +21,24 @@ class App extends Component {
 
     }
   ]
+
+  // function product(detail) = detail => {
+  //   return Product(detail);  
+  // }
     return (
         <Router>
+          
           <div className="app">
           <Header />
           <Switch>
+            {/* <NoMatch component={Home} /> */}
+            <Redirect from='/goods' to='/products' />
             <Route exact path='/' component={Home} />
-            <Route  path='/my-profile' component={MyProfile} />
-            <Route  path='/about-us' component={AboutUs} />
-            <Route  path='/products' component={Products} />
-            <Route  path='/products/1' component={Product(products[0])} />
+            <Route exact path='/my-profile' component={MyProfile} />
+            <Route exact path='/about-us' component={AboutUs} />
+            <Route exact path='/products' component={Products} />
+            <Route path="*" component={Home} />
+            // {/* <Route  path='/products/1' component={this.product(products[0])} /> */}
           </Switch>
           </div>
         </Router>
